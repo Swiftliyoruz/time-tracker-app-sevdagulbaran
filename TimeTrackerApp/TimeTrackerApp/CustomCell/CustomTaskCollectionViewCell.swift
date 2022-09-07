c3eb32adee9cf710a3add031090ee76649d6b176
sevdagulbaran <74216005+sevdagulbaran@users.noreply.github.com>
2022-09-08 00:03:52 +0300

    Created home ui

---
+++ TimeTrackerApp/TimeTrackerApp/CustomCell/CustomTaskCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTaskCollectionViewCell.swift
+//  TimeTrackerApp
+//
+//  Created by Sevda Gul Baran on 6.09.2022.
+//
+
+import UIKit
+
+class CustomTaskCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
